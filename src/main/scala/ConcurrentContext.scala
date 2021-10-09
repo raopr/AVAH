db12aa523e26db7eb4ac1ca82578797f4ef4cb76
raopr <raopr@umkc.edu>
2021-10-09 01:38:50 -0500

    Refactoring code

--- src/main/scala/ConcurrentContext.scala
+++ src/main/scala/ConcurrentContext.scala
@@ -3,7 +3,8 @@
  * 2020
  */
 
-// Futures code is taken from http://www.russellspitzer.com/2017/02/27/Concurrency-In-Spark/
+// Futures/sliding window code is taken from http://www.russellspitzer.com/2017/02/27/Concurrency-In-Spark/
+// Thanks to Russell Spitzer for excellent examples!
 object ConcurrentContext {
   import scala.util._
   import scala.concurrent._
@@ -17,14 +18,13 @@ object ConcurrentContext {
 
   /** Awaits only a set of elements at a time. At most batchSize futures will ever
    * be in memory at a time*/
-  def awaitBatch[T](it: Iterator[Future[T]], batchSize: Int = 3, timeout: Duration = Inf) = {
+  def awaitBatch[T](it: Iterator[Future[T]], batchSize: Int = 2, timeout: Duration = Inf) = {
     it.grouped(batchSize)
       .map(batch => Future.sequence(batch))
       .flatMap(futureBatch => Await.result(futureBatch, timeout))
   }
 
-  def awaitSliding[T](it: Iterator[Future[T]], batchSize: Int = 3, timeout: Duration = Inf): Iterator[T] = {
-    //val slidingIterator = it.sliding(batchSize - 1).withPartial(true) //Our look ahead (hasNext) will auto start the nth future in the batch
+  def awaitSliding[T](it: Iterator[Future[T]], batchSize: Int = 2, timeout: Duration = Inf): Iterator[T] = {
     val slidingIterator = it.sliding(batchSize - 1) //Our look ahead (hasNext) will auto start the nth future in the batch
     val (initIterator, tailIterator) = slidingIterator.span(_ => slidingIterator.hasNext)
     initIterator.map( futureBatch => Await.result(futureBatch.head, timeout)) ++
@@ -32,7 +32,7 @@ object ConcurrentContext {
   }
 
   // Switch between sliding window vs batch
-  def await[T](it: Iterator[Future[T]], batchSize: Int = 3, timeout: Duration = Inf) = {
+  def await[T](it: Iterator[Future[T]], batchSize: Int = 2, timeout: Duration = Inf) = {
     val ONE = 1
     if (batchSize.equals(ONE)) {
       awaitBatch(it, batchSize)
